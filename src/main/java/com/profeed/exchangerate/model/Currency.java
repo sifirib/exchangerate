2615404636dd2fba59d58024e300605101cc80fc
hsrv <sifirib01@gmail.com>
2024-02-09 03:23:12 +0300

    Currency model represents a postgresql table now.

--- src/main/java/com/profeed/exchangerate/model/Currency.java
+++ src/main/java/com/profeed/exchangerate/model/Currency.java
@@ -2,16 +2,32 @@ package com.profeed.exchangerate.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-
 import java.util.Map;
+import java.time.LocalDate;
 
 @Data
+@Entity
+@Table(name = "exchange_rates")
 public class Currency {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
     private boolean success;
-    private long timestamp;
+
+    @Column(nullable = false)
+    private Long timestamp;
+
+    @Column(nullable = false)
     private String base;
-    private String date;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "currency_rates", joinColumns = @JoinColumn(name = "exchange_rate_id"))
+    @MapKeyColumn(name = "currency")
+    @Column(name = "rate")
     private Map<String, Double> rates;
 }
-
-
